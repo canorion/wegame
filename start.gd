f5362623b44423797b0098d602aa900cb7f93120
burak-yucel-ext-orion <108812423+burak-yucel-ext-orion@users.noreply.github.com>
2024-01-29 22:09:59 +0300

    remove start scene

--- start.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ENTER:
-			get_tree().change_scene_to_file("res://stage.tscn")
